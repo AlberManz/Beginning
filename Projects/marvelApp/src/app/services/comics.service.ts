dcab903d39b09d5bcafa4063255473bea92a65d2
Alberto Manzano <114395777+AlberManz@users.noreply.github.com>
2022-12-30 18:01:03 +0100

    character view + intento modal

--- Projects/marvelApp/src/app/services/comics.service.ts
+++ Projects/marvelApp/src/app/services/comics.service.ts
@@ -10,11 +10,15 @@ export class ComicsService {
 
 public_key = 'c7e6dcca22b55b6ba87c2eee1a1c6b88';
 hash = '0afff71e24fff4c59b07b534b215481f';
-baseUrl = `https://gateway.marvel.com:443/v1/public/comics?limit=100&offset=1100&apikey=${this.public_key}&hash=${this.hash}`;
+baseUrl = `https://gateway.marvel.com:443/v1/public/`;
 
   constructor(private httpClient: HttpClient) { }
 
   getAllComics (): Observable<Comic> {
-    return this.httpClient.get<Comic>(this.baseUrl)
+    return this.httpClient.get<Comic>(`${this.baseUrl}comics?limit=100&offset=1100&apikey=${this.public_key}&hash=${this.hash}`)
+  }
+
+  getComicsByCharacter (pId: number): Observable<Comic> {
+    return this.httpClient.get<Comic>(`${this.baseUrl}characters/${pId}/comics?apikey=${this.public_key}&hash=${this.hash}`)
   }
 }
--- Projects/marvelApp/src/app/services/series.service.ts
+++ Projects/marvelApp/src/app/services/series.service.ts
@@ -10,11 +10,15 @@ export class SeriesService {
 
 public_key = 'c7e6dcca22b55b6ba87c2eee1a1c6b88';
 hash = '0afff71e24fff4c59b07b534b215481f';
-baseUrl = `https://gateway.marvel.com:443/v1/public/series?limit=100&offset=1100&apikey=${this.public_key}&hash=${this.hash}`;
+baseUrl = `https://gateway.marvel.com:443/v1/public/`;
 
   constructor(private httpClient: HttpClient) { }
 
   getAllSeries (): Observable<Serie> {
-    return this.httpClient.get<Serie>(this.baseUrl)
+    return this.httpClient.get<Serie>(`${this.baseUrl}series?limit=100&offset=1100&apikey=${this.public_key}&hash=${this.hash}`)
+  }
+
+  getSeriesByCharacter (pId: number): Observable<any> {
+    return this.httpClient.get<Serie>(`${this.baseUrl}characters/${pId}/series?apikey=${this.public_key}&hash=${this.hash}`)
   }
 }
